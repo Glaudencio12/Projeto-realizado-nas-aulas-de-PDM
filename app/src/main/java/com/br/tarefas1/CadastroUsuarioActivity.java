ff23c90496cfcab26a502302c128b4bac8901ff9
Bruno Vicente <brunnovicente@gmail.com>
2024-12-04 16:19:20 -0300

    Conexão com banco e classe Usuário

--- app/src/main/java/com/br/tarefas1/CadastroUsuarioActivity.java
+++ app/src/main/java/com/br/tarefas1/CadastroUsuarioActivity.java
@@ -1,9 +1,6 @@
 package com.br.tarefas1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class MainActivity extends AppCompatActivity {
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro_usuario);
 
     }
-
-    public void abrirCadastro(View v){
-        Intent tela_cadastro = new Intent(this, CadastroUsuarioActivity.class);
-        startActivity(tela_cadastro);
-    }
-
 }
---
+++ app/src/main/java/persistencia/Usuario.java
@@ -0,0 +1,73 @@
+package persistencia;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Usuario {
+
+    private SQLiteOpenHelper bancoHelper;
+    private SQLiteDatabase banco;
+
+    private int id;
+    private String nome;
+    private String login;
+    private String senha;
+    private int categoria;
+
+    public Usuario(Context context){
+        bancoHelper = new Banco(context);
+        banco = bancoHelper.getWritableDatabase();
+    }
+
+    public void cadastrar(){
+        ContentValues conteudo = new ContentValues();
+        conteudo.put("nome", this.nome);
+        conteudo.put("login", this.login);
+        conteudo.put("senha", this.senha);
+        conteudo.put("categoria", this.categoria);
+
+        banco.insert("usuarios", null, conteudo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+}
